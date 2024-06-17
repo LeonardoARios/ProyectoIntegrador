9d206f5db9bdbf0d02cb313f54758db03180c9dc
LeonardoARios <leon82river@gmail.com>
2024-06-17 18:01:02 -0300

    Se centra elementos, se agrega nuevo menu de seleccion, se agrega funcion y codigo de color

--- codigoInicial.py
+++ codigoInicial.py
@@ -8,9 +8,6 @@ import os
 import colorama
 from colorama import Back,Fore,Style #dejo agregado esto x si usamos los estilos
 
-
-
-
 def limpiar_Pantalla():
     """ 
     La funcion limpia la pantalla 
@@ -28,31 +25,6 @@ def limpiar_Pantalla():
     else:
        os.system("clear")
 
-
-def menu():
-    """
-    La funcion menú
-    Muestra en pantalla las diferentes opciones que se muestran al usuario
-    Parametros: 
-    No recibe
-    Retorna: 
-    Un valor entero para seleccion en el Match
-    AUTOR: Marina Toledo, Ale Ante, Leo Rios, Brenda Sztryk
-    COLABORADORES:
-    """
-    limpiar_Pantalla()
-    print(colorama.Fore.CYAN)
-    print(" SYS PETS ".center(50,"*").center(100))
-    print(("="*70).center(100))
-    print("\t\t 1. Agregar Paciente")
-    print("\t\t 2. Listar")
-    print("\t\t 3. Salir")
-    print(("="*70).center(100))
-    op = int(input("\t\t Ingresa la Opción:  "))
-    limpiar_Pantalla()
-    return op
-
-
 def listar(lista):
     """
     La Función da como retorno una un listado de los datos que se guardaron.
@@ -60,37 +32,97 @@ def listar(lista):
     Un dato en forma de lista y diccionarios.
     Retorna: 
     Muestra en pantalla. 
-    AUTOR: Marina Toledo, Ale Ante, Leo Rios, Brenda Sztryk
-    COLABORADORES:
+    AUTOR: Leonardo Rios
+    COLABORADORES: Marina Toledo, Ale Ante, Brenda Sztryk
     """
-    print(("-"*70).center(100))
-    #como es una lista veo primero que tiene adentro
+    carti = 1
+    print(colorama.Fore.BLUE)
+    print(f'{"-"*35}{carti}{"-"*35}'.center(100))
     for paciente in lista:
-    #como veo que tiene diccionarios, itero a los diccionarios con paciente.items()
         for dato, info in paciente.items():
-            #como no me gusta que se vea Mascotas: []entre corchetes uso continue
             if dato == "Mascotas":
                 continue
             print(f'\t\t{dato}: {info}')
-        #Luego itero solamente la lista que da como resultado "Mascotas" y lo itero con
-        #end="" para que quede mas prolijo
         for dato in paciente:
             if dato == "Mascotas":
                 print("\t\tMascotas: ", end="")
                 for info in info:
                     print(f' {info}', end="")
                 print()
-        print(("-"*70).center(100))
+        if carti < len(lista):
+            carti = carti +1
+            print(f'{"-"*35}{carti}{"-"*35}'.center(100))
+    print(("-"*70).center(100))
+    input("\t\tPresione Enter para continuar")
+    limpiar_Pantalla()
     return
 
-'''def presentar():
-    """ funcion de bienvenida al programa
-    no recibe parametro, retorna nombre del programa
-    AUTOR: Brenda Sztryk
-    COLABORADORES: """
-    print (" SYS PETS ".center(50,"*").center(100)) # ESTA FRASE PENSE SE ESCRIBA CON ASTERISCOS O X LO MENOS "PETS" :)
-    return'''
+def modificarD(lista):
+    '''
+    Función que muestra la cantidad de datos guardados, enumera las cartillas y da la opción de 
+    que items se quiere cambiar. 
+    Parametros: 
+    Espera una lista e itera diccionario
+    Retorna: 
+    Lista modificada para la opcion "Listar"
+    Autor: Leonardo RIos
+    colaboradores: Marina Toledo, Ale Ante, Leo Rios, Brenda Sztryk
+    '''
+    carti = 1
+    print(colorama.Fore.YELLOW)
+    print(f'\t\tNumero de datos cargados: {len(lista)}')
+    print(colorama.Fore.RESET)
+    print(f'{"-"*35}{carti}{"-"*35}'.center(100))
+    for deDic in lista:
+        for dato,infos in deDic.items():
+            if dato == "Mascotas":
+                continue
+            else:
+                print(f'\t\t{dato}: {infos}')
+        for dato in deDic:
+            if dato == "Mascotas":
+                print("\t\tMascotas:  ", end="")
+                for info in infos: 
+                    print(f'{info}, ', end="")
+                print()
+        if carti < len(lista):
+            carti = carti +1
+            print(f'{"-"*35}{carti}{"-"*35}'.center(100))
 
+    respo1 = int(input(colorama.Fore.YELLOW + "\t\tQue cartilla desea cambiar?:  ".upper() + colorama.Fore.RESET))
+    respo1 = respo1 - 1 
+    bande = True
+    while bande:
+        if respo1 >= 0 and respo1 <= len(lista):
+                if lista[respo1] in lista:
+                    respo2 = input(colorama.Fore.YELLOW + "\t\tQue item desea cambiar?:  ".upper() + colorama.Fore.RESET) 
+                    while bande:
+                        if respo2 in lista[respo1]:
+                            if respo2 == "Mascotas":
+                                for dato in deDic:
+                                    if dato == "Mascotas":
+                                        print("\t\tMascotas:  ", end="")
+                                        num = 0
+                                        for info in infos:
+                                            num = num +1
+                                            print(f'{num}:{info}, ', end="")
+                                        print()
+                                nuModifico = int(input(colorama.Fore.YELLOW + "\t\tIngrese el número que desea modificar".upper())+colorama.Fore.RESET)
+                                nueMascota = input(colorama.Fore.YELLOW + "\t\tIngrese la nueva mascota:  ".upper() + colorama.Fore.RESET)
+                                deDic[dato][nuModifico]=nueMascota
+                                bande = False
+                            else:    
+                                respo3 = input(colorama.Fore.YELLOW + "\t\tEscriba el nuevo valor:  ".upper() + colorama.Fore.RESET)
+                                lista[respo1][respo2]=respo3
+                                bande = False
+                        else:
+                            print(colorama.Fore.RED + "\t\tItem ingresado no valido".upper() + colorama.Fore.RESET)
+                            respo2 = input(colorama.Fore.YELLOW + "\t\tQue item desea cambiar?:  ".upper() + colorama.Fore.RESET)
+        else: 
+            print(colorama.Fore.RED + "\t\tNumero ingresado fuera de rango".upper() + colorama.Fore.RESET)
+            respo1 = int(input(colorama.Fore.YELLOW + "\t\tQue cartilla desea cambiar?:  ".upper()) + colorama.Fore.RESET)
+    return lista
+            
 def agregar(paci):
     """
     Función agregar 
@@ -99,21 +131,23 @@ def agregar(paci):
     Datos ingresados de caracteres string y entero
     Retorna: 
     Dato en forma de lista empaquetada, que incluye por cada usuario un diccionario. 
+    Autor: Leonardo Ríos
+    Colaboradores: Marina Toledo, Ale Ante, Leo Rios, Brenda Sztryk
     """
-
     masco = []
     print(("-"*70).center(100))
-    nomPaci = input("\t\tIngrese el nombre:  ")
-    apellPaci = input("\t\tIngrese el apellido:  ")
-    dniPaci = input("\t\tIngrese el DNI:  ")
-    direPaci = input("\t\tIngrese la dirección:  ")
-    respuesta = input("\t\tIngrese el nombre de la mascota:  ")
+    nomPaci = input(colorama.Fore.GREEN + "\t\tIngrese el nombre:  " + colorama.Fore.RESET)
+    apellPaci = input(colorama.Fore.GREEN + "\t\tIngrese el apellido:  " + colorama.Fore.RESET)
+    dniPaci = input(colorama.Fore.GREEN + "\t\tIngrese el DNI:  " + colorama.Fore.RESET)
+    direPaci = input(colorama.Fore.GREEN + "\t\tIngrese la dirección:  " + colorama.Fore.RESET)
+    respuesta = input(colorama.Fore.GREEN + "\t\tIngrese el nombre de la mascota:  " + colorama.Fore.RESET)
     masco.append(respuesta)
-    respo = input("\t\tQuiere ingresar otra mascota? s/n:  ")
+    respo = input(colorama.Fore.GREEN + "\t\tQuiere ingresar otra mascota? s/n:  " + colorama.Fore.RESET)
     while respo == "s":
-        respuesta = input("\t\tIngrese la otra mascota:  ")
+        respuesta = input(colorama.Fore.GREEN + "\t\tIngrese la otra mascota:  " + colorama.Fore.RESET)
         masco.append(respuesta)
-        respo = input("Quiere ingresar otra mascota? s/n:  ")
+        respo = input(colorama.Fore.GREEN + "\t\tQuiere ingresar otra mascota? s/n:  " + colorama.Fore.RESET)
+    
     paci = {
         "Nombre": nomPaci,
         "Apellido": apellPaci,
@@ -123,25 +157,82 @@ def agregar(paci):
         }
     nombre.append(paci)
 
+def menu():
+    """
+    La funcion da opciones a seleccionar x el usuario
+    no espera argumentos y retorna la opcion elegida x usuario.
+    AUTOR: Brenda Sztryk
+    COLABORADORES: Marina Toledo, Ale Ante, Leo Rios
+    """
+    print()
+    print((Style.BRIGHT + "*" * 40).center(100) )
+    print (" MENU ".center(20).center(100))
+    print((Style.BRIGHT + "*" * 40).center(100) )
+    print(Style.RESET_ALL) #RESETEA LOS EFECTOS VISUALES DE ACA HACIA ABAJO, HASTA VOLVER A APLICARLO
+    print(("SELECCIONE LETRA PARA OPCION DESEADA").center(40).center(100))
+    print()
+    print((Fore.LIGHTMAGENTA_EX + "\t A" + Fore.RESET + " - Nuevo paciente").center(100))
+    print((Fore.LIGHTMAGENTA_EX + "\t B" + Fore.RESET + " - Buscar paciente").center(100))
+    print((Fore.LIGHTMAGENTA_EX + "\t C" + Fore.RESET + " - Eliminar paciente").center(100))
+    print((Fore.LIGHTMAGENTA_EX + "\t D" + Fore.RESET + " - Modificar base datos").center(100))
+    print((Fore.LIGHTMAGENTA_EX + "\t E" + Fore.RESET + " - Listar pacientes").center(100))
+    print((Fore.LIGHTMAGENTA_EX + "\t F" + Fore.RESET + " - Presupuestar").center(100)) #aca un modelo de ticket o factura
+    print((Fore.LIGHTMAGENTA_EX + "\t G" " - SALIR").center(90))
+    print()
+    print((Fore.RESET + "*" * 40).center(100))
+    #print(Style.RESET_ALL)
+    op_elegida = input("\t\t\t\tIngrese letra seleccionada: ").lower()
+    limpiar_Pantalla()
+    return op_elegida
+
 #Programa Principal
 nombre = []
 pacientes = {}
+bande = True
 op = menu()
-while op != 4:
+while bande == True:
     match op:
-        case 1:
-            print()
-            print("\t\tUsted seleccionó Agregar")
+        case "a": #Nuevo Paciente
+            print(colorama.Fore.GREEN + "\t\tUsted seleccionó Agregar" + colorama.Fore.RESET)
             agregar(pacientes)
+            print(("-"*70).center(100))
+            print(colorama.Fore.RESET)
             input("\t\tPresione enter para continuar")
-        case 2:
+            
+        case "b": #Buscar Paciente
             print()
-            print("\t\tUsted seleccionó Listar")
+            print()
+            print("\t\tUsted seleccionó Buscar Paciente")
+            #En Espera
+            input("\t\tPresione enter para continuar")
+
+        case "c": #Eliminar Paciente
+            print()
+            print("\t\tUsted seleccionó Eliminar Paciente")
+            #En Espera
+            input("\t\tPresione enter para continuar")
+        
+        case "d": #Modificar Dato
+            print(colorama.Fore.YELLOW + "\t\tUsted seleccionó Modificar" + colorama.Fore.RESET)
+            modificarD(nombre)
+            print(("-"*70).center(100))
+            print(colorama.Fore.RESET)
+            input("\t\tPresione enter para continuar")
+
+        case "e": #Listar Paciente
+            print(colorama.Fore.BLUE + "\t\tUsted seleccionó Listar" + colorama.Fore.RESET)
             listar(nombre)
-            input("\t\tPresione enter para continuar")
-        case 3:
+            print(colorama.Fore.RESET)
+        
+        case "f":#Presupuestar
             print()
-            print("\t\tSALDRA DEL PROGRAMA")
+            print("\t\tUsted seleccionó Presupuestar")
+            #En Espera
+            input("\t\tPresione enter para continuar")
+
+        case "g": #Salir
+            print(("-"*70).center(100))
+            print("\t\tUsted seleccionó Salir")
             break
         case _:
             print("\t\tValor invalido")
